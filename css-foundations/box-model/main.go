60692f4aef81ca6342364cf9c240d53bb6dce946
Scott Bergler <commandinghands@gmail.com>
2020-06-24 06:44:11 -0700

    Initialize box model lesson.

---
+++ css-foundations/box-model/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("./site/*html"))
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("./site"))
+	http.Handle("/", fs)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
