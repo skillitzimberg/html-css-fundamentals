5e99591218ef0a96e8a6ecd2542fe4f9da2400e3
Scott Bergler <commandinghands@gmail.com>
2020-06-24 07:38:43 -0700

    Box sizing with borders and padding.

--- css-foundations/box-model/main.go
+++ css-foundations/box-model/main.go
@@ -15,9 +15,14 @@ func init() {
 func main() {
 	fs := http.FileServer(http.Dir("./site"))
 	http.Handle("/", fs)
+	http.HandleFunc("/border", border)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func border(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "border.html", nil)
 }
